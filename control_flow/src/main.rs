8df322740fc674669af3b5793dd52d35eb13114b
jione <wldnjs9935@naver.com>
2018-07-02 19:05:08 +0900

    ownership

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 6;
+
+    if number % 4 == 0{
+        println!("number is divisible by 4");
+    }
+    else if number % 3 == 0{
+        println!("number is divisible by 3");
+    }
+    else if number % 2 == 0{
+        println!("number is divisible by 2");
+    }
+    else{
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let num = if condition {
+        5
+    }
+    else {
+        6
+    };
+
+    println!("The value of number is:{}", num);
+
+    let mut while_num = 3;
+    while while_num != 0{
+        println!("{}!", while_num);
+
+        while_num = while_num - 1;
+    }
+    println!("LIFTOFF!!!");
 }
